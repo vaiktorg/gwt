ce0e01e9da7a18061cd1120eb65f1e489de448b6
victor <vaiktorg>
2022-04-12 22:18:50 -0400

    initial commit

---
+++ gwt.go
@@ -0,0 +1,207 @@
+package gwt
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"github.com/google/uuid"
+	"strings"
+	"time"
+)
+
+var _Spice Spice
+
+// "import _ github.com/vaiktorg/gwt" to initialize the default global _Spice encryption flavoring
+func init() {
+	_Spice.Salt = []byte(uuid.New().String())
+	_Spice.Salt = []byte(uuid.New().String())
+}
+
+type Spice struct {
+	Salt   []byte
+	Pepper []byte
+}
+
+type (
+	Header struct {
+		ID        string
+		Issuer    string
+		Timestamp time.Time
+	}
+
+	GWT struct {
+		Header    Header
+		Payload   interface{} // Use type assertion to get your expected payload package.
+		Signature []byte      // Token Signature
+		spice     Spice
+	}
+)
+
+const (
+	TokenName = "gwt"
+)
+
+const (
+	ErrorMissingParts      = "parts of token are missing"
+	ErrorNoTokProvided     = "no token provided"
+	ErrorFailedToDecode    = "failed to decode token signature"
+	ErrorFailedToEncodeSig = "failed to encode token signature"
+	ErrorFailedToEncodePL  = "failed to encode token payload"
+	ErrorFailedToEncodeHdr = "failed to encode token header"
+	ErrorNilArgument       = "argument payload is nil"
+	ErrorSignatureNotMatch = "signatures do not match"
+)
+
+func NewDefaultGWT(issuer string) *GWT {
+	t := &GWT{
+		Header: Header{
+			ID:        uuid.NewString(),
+			Issuer:    issuer,
+			Timestamp: time.Now(),
+		},
+		Payload: "",
+		spice: Spice{
+			Salt:   _Spice.Salt,
+			Pepper: _Spice.Pepper,
+		},
+	}
+	return t
+}
+
+func NewGWTWithSpice(issuer string, spice Spice) *GWT {
+	t := &GWT{
+		Header: Header{
+			ID:        uuid.NewString(),
+			Issuer:    issuer,
+			Timestamp: time.Now(),
+		},
+		Payload: "",
+		spice:   spice,
+	}
+	return t
+}
+
+func (g *GWT) Encode(payload interface{}) (token, signature string, err error) {
+	if payload == nil {
+		return "", "", errors.New(ErrorNilArgument)
+	}
+
+	// Payload JSON string
+	payloadBuffer := new(bytes.Buffer)
+	err = json.NewEncoder(payloadBuffer).Encode(payload)
+	if err != nil {
+		return "", "", errors.New(ErrorFailedToEncodePL)
+	}
+
+	// Header JSON string
+	headerBuffer := new(bytes.Buffer)
+	err = json.NewEncoder(headerBuffer).Encode(&Header{
+		ID:        g.Header.ID,
+		Issuer:    g.Header.Issuer,
+		Timestamp: time.Now(),
+	})
+
+	if err != nil {
+		return "", "", errors.New(ErrorFailedToEncodeHdr)
+	}
+
+	// ----------------------------------------------------------------------------------------------
+	// Signature Encoding
+	hash := hmac.New(sha256.New, g.spice.Salt)
+	_, err = hash.Write(append(headerBuffer.Bytes(), payloadBuffer.Bytes()...))
+	if err != nil {
+		return "", "", errors.New(ErrorFailedToEncodeSig)
+	}
+
+	hashSignature := hash.Sum(g.spice.Pepper)
+
+	b64signature := make([]byte, base64.URLEncoding.EncodedLen(len(hashSignature)))
+	base64.URLEncoding.Encode(b64signature, hashSignature)
+
+	// ----------------------------------------------------------------------------------------------
+	// Payload Encoding
+	b64payload := make([]byte, base64.URLEncoding.EncodedLen(payloadBuffer.Len()))
+	base64.URLEncoding.Encode(b64payload, payloadBuffer.Bytes())
+
+	//----------------------------------------------------------------------------------------------
+	// Header Encoding
+	b64header := make([]byte, base64.URLEncoding.EncodedLen(headerBuffer.Len()))
+	base64.URLEncoding.Encode(b64header, headerBuffer.Bytes())
+	//-----------
+
+	// Results in token "b64Header.b64Payload.b64Signature"
+	return strings.Join([]string{
+			string(b64header),
+			string(b64payload),
+			string(b64signature),
+		}, "."),
+		string(b64signature),
+		nil
+}
+
+func (g *GWT) Decode(tkn string) error {
+	if tkn == "" {
+		return errors.New(ErrorNoTokProvided)
+	}
+
+	// Split Message
+	gwtParts := strings.Split(tkn, ".")
+	if len(gwtParts) > 3 || len(gwtParts) < 3 {
+		return errors.New(ErrorMissingParts)
+	}
+
+	// ------------------------------------------------------------------------------------------------
+	// B64 Decoding
+	// Signature
+	signatureBuff, err := base64.URLEncoding.DecodeString(gwtParts[2])
+	if err != nil {
+		return err
+	}
+
+	// Payload
+	payloadBuff, err := base64.URLEncoding.DecodeString(gwtParts[1])
+	if err != nil {
+		return err
+	}
+
+	// Header
+	headerBuff, err := base64.URLEncoding.DecodeString(gwtParts[0])
+	if err != nil {
+		return err
+	}
+
+	// ------------------------------------------------------------------------------------------------
+	// Decode data
+	err = json.NewDecoder(bytes.NewReader(headerBuff)).Decode(&g.Header)
+	if err != nil {
+		return errors.New(ErrorFailedToDecode)
+	}
+
+	err = json.NewDecoder(bytes.NewReader(payloadBuff)).Decode(&g.Payload)
+	if err != nil {
+		return errors.New(ErrorFailedToDecode)
+	}
+
+	// ------------------------------------------------------------------------------------------------
+	// Signature Validation
+
+	//Generate gwt Signature from decoded payload
+	hash := hmac.New(sha256.New, g.spice.Salt)
+	_, err = hash.Write(append(headerBuff, payloadBuff...))
+	if err != nil {
+		return errors.New(ErrorFailedToDecode)
+	}
+
+	hashSignature := hash.Sum(g.spice.Pepper)
+
+	// Validate
+	if !bytes.Equal(hashSignature, signatureBuff) {
+		return errors.New(ErrorSignatureNotMatch)
+	}
+
+	g.Signature = hashSignature
+	return nil
+}
---
+++ gwt_test.go
@@ -0,0 +1,75 @@
+package gwt
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"testing"
+)
+
+var (
+	GlobalIssuer    = "CompanyIssuerName"
+	GlobalToken     = ""
+	GlobalSignature = ""
+)
+
+var Gwt = NewDefaultGWT(GlobalIssuer)
+var GwtWith = NewGWTWithSpice(GlobalIssuer, Spice{
+	Salt:   []byte(uuid.New().String()),
+	Pepper: []byte(uuid.New().String()),
+})
+
+func TestGWT(t *testing.T) {
+	t.Run("Encode", func(t *testing.T) {
+		token, signature, err := Gwt.Encode("this is a string payload")
+		if err != nil {
+			t.Error(err)
+			t.Failed()
+		}
+
+		GlobalToken = token
+		GlobalSignature = signature
+
+		fmt.Printf("%#+v\n", Gwt)
+		fmt.Printf("%#+v\n", GlobalToken)
+		fmt.Printf("%#+v\n", GlobalSignature)
+
+	})
+
+	t.Run("Decode", func(t *testing.T) {
+		err := Gwt.Decode(GlobalToken)
+		if err != nil {
+			t.Error(err)
+			t.Failed()
+		}
+
+		fmt.Printf("%#+v\n", Gwt)
+
+	})
+
+	t.Run("EncodeWith", func(t *testing.T) {
+		token, signature, err := GwtWith.Encode("this is a string payload")
+		if err != nil {
+			t.Error(err)
+			t.Failed()
+		}
+
+		GlobalToken = token
+		GlobalSignature = signature
+
+		fmt.Printf("%#+v\n", Gwt)
+		fmt.Printf("%#+v\n", GlobalToken)
+		fmt.Printf("%#+v\n", GlobalSignature)
+
+	})
+
+	t.Run("DecodeWith", func(t *testing.T) {
+		err := GwtWith.Decode(GlobalToken)
+		if err != nil {
+			t.Error(err)
+			t.Failed()
+		}
+
+		fmt.Printf("%#+v\n", Gwt)
+
+	})
+}
